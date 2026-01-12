b46e8f870a5087ab82320e4fa9fdda4158170fdc
ItsSteven244 <steven.alexis21@gmail.com>
2026-01-11 21:30:42 -0500

    admin por defecto

--- src/main.js
+++ src/main.js
@@ -3,4 +3,24 @@ import App from "./App.vue";
 import router from "./router";
 import "./styles/Global.css";
 
+// ===== ADMIN POR DEFECTO (SI NO HAY USUARIOS) =====
+const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+if (!usuarios || usuarios.length === 0) {
+  localStorage.setItem(
+    "usuarios",
+    JSON.stringify([
+      {
+        id: 1,
+          usuario: "e1311547390@live.uleam.edu.ec",
+          password: "admin1",
+          nombre: "Alex",
+          rol: "Admin",
+          estado: "Activo"
+      }
+    ])
+  );
+}
+// ================================================
+
 createApp(App).use(router).mount("#app");
